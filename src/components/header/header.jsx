f7fe286dc84ede467d7e6df0219dbffc680d4c3e
AlexGnutov <81751916+AlexGnutov@users.noreply.github.com>
2022-06-14 12:02:43 +0300

    Links added

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,16 @@
-import HeaderNavBar from "./components/header-nav-bar";
+import React from 'react';
+import HeaderNavBar from './components/header-nav-bar';
 
 function Header() {
-    return (
-        <header className="container">
-            <div className="row">
-                <div className="col">
-                    <HeaderNavBar/>
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header className="container">
+      <div className="row">
+        <div className="col">
+          <HeaderNavBar />
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
